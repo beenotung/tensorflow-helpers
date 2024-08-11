4dd1d263cfd2c2ee959c8f8c5617879968c332c5
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-08-11 14:48:52 +0800

    feat: export classifier in index

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
 import * as tfjs from '@tensorflow/tfjs-node'
 export * from './image'
 export * from './model'
+export * from './classifier'
 
 export let tensorflow = tfjs
 export let tf = tensorflow
