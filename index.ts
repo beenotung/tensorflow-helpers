167ea40d2f1ffdd295cb2f5e97d5a19aac756aac
Beeno Tung <aabbcc1241@yahoo.com.hk>
2024-08-22 10:57:54 +0800

    patch: export file helper functionst

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ export * from './image'
 export * from './model'
 export * from './classifier'
 export * from './tensor'
+export * from './file'
 
 export let tensorflow = tfjs
 export let tf = tensorflow
