419ada08e916e28dfeecfeba7f9771bb5bc67818
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-09-23 00:33:40 +0800

    wip: add CAM (class activation map)

---
+++ cam.test.ts
@@ -0,0 +1,265 @@
+import * as tf from '@tensorflow/tfjs'
+import { loadImageClassifierModel, loadImageModel } from './browser'
+
+console.log('cam.test.ts')
+
+let catImage = document.getElementById('catImage') as HTMLImageElement
+let dogImage = document.getElementById('dogImage') as HTMLImageElement
+let startCameraButton = document.getElementById(
+  'startCameraButton',
+) as HTMLButtonElement
+let startVideoButton = document.getElementById(
+  'startVideoButton',
+) as HTMLButtonElement
+let video = document.getElementById('video') as HTMLVideoElement
+let inputCanvas = document.getElementById('inputCanvas') as HTMLCanvasElement
+let camCanvas = document.getElementById('camCanvas') as HTMLCanvasElement
+
+let classItems = Array.from(
+  document.querySelectorAll<HTMLElement>('.class-item'),
+)
+
+let inputContext = inputCanvas.getContext('2d')!
+let camContext = camCanvas.getContext('2d')!
+
+let size = 224
+inputCanvas.width = size
+inputCanvas.height = size
+camCanvas.width = size
+camCanvas.height = size
+
+classItems.forEach(item => {
+  item.onclick = async () => {
+    classItems.forEach(item => {
+      item.classList.remove('active')
+    })
+    item.classList.add('active')
+  }
+})
+
+let brightnessRate = 1
+
+startCameraButton.onclick = async () => {
+  brightnessRate = 4
+  video.controls = false
+  await startCamera()
+}
+startVideoButton.onclick = async () => {
+  brightnessRate = 1
+  video.controls = true
+  await startVideo()
+}
+
+catImage.onclick = async () => {
+  await analyzeImage(catImage)
+}
+dogImage.onclick = async () => {
+  await analyzeImage(dogImage)
+}
+
+async function analyzeImage(image: HTMLImageElement) {
+  let inputSize = Math.min(image.naturalWidth, image.naturalHeight)
+  let left = (image.naturalWidth - inputSize) / 2
+  let top = (image.naturalHeight - inputSize) / 2
+  inputContext.drawImage(
+    image,
+    left,
+    top,
+    inputSize,
+    inputSize,
+    0,
+    0,
+    inputCanvas.width,
+    inputCanvas.height,
+  )
+  let imageData = inputContext.getImageData(
+    0,
+    0,
+    inputCanvas.width,
+    inputCanvas.height,
+  )
+  await analyze(imageData)
+}
+
+async function loopVideo() {
+  // crop to center
+  let inputSize = Math.min(video.videoWidth, video.videoHeight)
+  let left = (video.videoWidth - inputSize) / 2
+  let top = (video.videoHeight - inputSize) / 2
+  inputContext.drawImage(
+    video,
+    left,
+    top,
+    inputSize,
+    inputSize,
+    0,
+    0,
+    inputCanvas.width,
+    inputCanvas.height,
+  )
+  let imageData = inputContext.getImageData(
+    0,
+    0,
+    inputCanvas.width,
+    inputCanvas.height,
+  )
+
+  let r_min = 255
+  let r_max = 0
+
+  let g_min = 255
+  let g_max = 0
+
+  let b_min = 255
+  let b_max = 0
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    let r = imageData.data[i]
+    let g = imageData.data[i + 1]
+    let b = imageData.data[i + 2]
+
+    r_min = Math.min(r_min, r)
+    r_max = Math.max(r_max, r)
+    g_min = Math.min(g_min, g)
+    g_max = Math.max(g_max, g)
+    b_min = Math.min(b_min, b)
+    b_max = Math.max(b_max, b)
+  }
+
+  let r_range = r_max - r_min
+  let g_range = g_max - g_min
+  let b_range = b_max - b_min
+  if (r_range == 0 || g_range == 0 || b_range == 0) {
+    // too dim or too bright
+    requestAnimationFrame(loopVideo)
+    return
+  }
+
+  let max = Number.NEGATIVE_INFINITY
+  let min = Number.POSITIVE_INFINITY
+
+  for (let i = 0; i < imageData.data.length; i += 4) {
+    let r = imageData.data[i]
+    let g = imageData.data[i + 1]
+    let b = imageData.data[i + 2]
+
+    max = Math.max(max, r, g, b)
+    min = Math.min(min, r, g, b)
+
+    // r = ((r - r_min) / r_range) * 255
+    // g = ((g - g_min) / g_range) * 255
+    // b = ((b - b_min) / b_range) * 255
+
+    r = Math.min(r * brightnessRate, 255)
+    g = Math.min(g * brightnessRate, 255)
+    b = Math.min(b * brightnessRate, 255)
+
+    r = Math.floor(r)
+    g = Math.floor(g)
+    b = Math.floor(b)
+
+    imageData.data[i] = r
+    imageData.data[i + 1] = g
+    imageData.data[i + 2] = b
+    imageData.data[i + 3] = 255
+  }
+  inputContext.putImageData(imageData, 0, 0)
+
+  console.log({ max, min })
+  debugger
+
+  await analyze(imageData)
+
+  requestAnimationFrame(loopVideo)
+}
+
+async function analyze(imageData: ImageData) {
+  let models = await modelsP
+  let tape = tf.grad(x => {
+    let embedding = models.baseModel.model.predict(x) as tf.Tensor
+    // return embedding.slice(0, 1)
+    // return embedding
+    let y = models.classifier.classifierModel.predict(embedding) as tf.Tensor
+    let mask = tf.tensor(
+      classItems.map(item => (item.classList.contains('active') ? 1 : 0)),
+    )
+    return tf.mul(y, mask)
+  })
+
+  let data = tf.tidy(() => {
+    let x = tf.browser.fromPixels(inputCanvas)
+    x = tf.div(x, 255)
+    x = tf.expandDims(x, 0)
+    let grad = tape(x)
+    let min = tf.min(grad)
+    let max = tf.max(grad)
+    let range = tf.sub(max, min)
+    let normalized = tf.div(tf.sub(grad, min), range)
+    // [224, 224, 3]
+    let data = normalized.squeeze().arraySync() as number[][][]
+    return data
+  })
+
+  let i = 0
+  let min = 255
+  let max = 0
+  for (let y = 0; y < imageData.height; y++) {
+    for (let x = 0; x < imageData.width; x++, i += 4) {
+      let r = Math.floor(data[y][x][0] * 255)
+      let g = Math.floor(data[y][x][1] * 255)
+      let b = Math.floor(data[y][x][2] * 255)
+      min = Math.min(min, r, g, b)
+      max = Math.max(max, r, g, b)
+      imageData.data[i] = r
+      imageData.data[i + 1] = g
+      imageData.data[i + 2] = b
+      imageData.data[i + 3] = 255
+    }
+  }
+  if (min != max) {
+    camContext.putImageData(imageData, 0, 0)
+  }
+}
+
+async function loadModels() {
+  let baseModel = await loadImageModel({
+    url: 'res/cat-dog/base_model',
+    cacheUrl: 'indexeddb://cat-dog-base-model',
+    checkForUpdates: false,
+  })
+  let classifier = await loadImageClassifierModel({
+    baseModel,
+    classNames: ['both', 'cat', 'dog', 'others'],
+    modelUrl: 'res/cat-dog/classifier_model',
+    cacheUrl: 'indexeddb://cat-dog-classifier-model',
+  })
+  return { baseModel, classifier }
+}
+
+let modelsP = loadModels()
+
+async function startCamera() {
+  let stream = await navigator.mediaDevices.getUserMedia({
+    video: true,
+  })
+  await new Promise<void>(resolve => {
+    video.onloadedmetadata = () => resolve()
+    video.srcObject = stream
+    video.play()
+  })
+
+  loopVideo()
+}
+
+async function startVideo() {
+  await new Promise<void>(resolve => {
+    video.onloadedmetadata = () => resolve()
+    let file =
+      'A dog, a cat, and a hamster who gets the last treat [3F5Cz_YN8FA].mp4'
+    video.src = 'dataset/' + file
+    video.play()
+  })
+  loopVideo()
+}
+
+// startCamera().catch(e => console.error(e))
